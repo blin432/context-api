862598e857239ebe3569e291192153dcf5e02233
benjamin lin <blin432@gmail.com>
2020-03-24 10:53:53 -0400

    first commit

---
+++ src/context/GlobalState.js
@@ -0,0 +1,42 @@
+import React, {createContext, useReducer} from 'react';
+import AppReducer from './AppReducer';
+
+
+//initial state
+
+const initialState ={
+    transactions:[]
+}
+
+//create content
+export const GlobalContext = createContext(initialState)
+
+//Provider Component
+export const GlobalProvider = ({children}) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState)
+
+
+    //actions
+    function deleteTransactions(id){
+        dispatch({
+            type:'DELETE_TRANSACTION',
+            payload:id
+        });
+    }
+
+    function addTransactions(transaction){
+        dispatch({
+            type:'ADD_TRANSACTION',
+            payload: transaction
+        });
+    }
+
+    
+    return(<GlobalContext.Provider value={{
+        transactions:state.transactions,
+        deleteTransactions,
+        addTransactions
+    }}>
+        {children}
+    </GlobalContext.Provider>)
+}
